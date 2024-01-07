bac3703b76290a09042548423658d9b9f6b06737
Koustav Das <kdkdas8@gmail.com>
2024-01-07 14:02:25 +0530

    updates

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+//Middlewares
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "Hello from the other side.", app: "welcome to the app" });
-// });
+app.use(morgan("dev"));
 
-// app.post("/", (req, res) => {
-//   res.status(201).send("You posted to this end point");
-// });
+app.use((req, res, next) => {
+  console.log("Hello from the Custom Middleware 🤟🏻");
+  next();
+});
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//IO Operations
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+//Controllers
+const getAllTours = (req, res) => {
+  console.log("API requested at:", req.requestTime);
   res.status(200).json({
     status: "success",
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
     },
   });
-});
+};
 
-app.get(`/api/v1/tours/:id`, (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -44,9 +53,9 @@ app.get(`/api/v1/tours/:id`, (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -62,9 +71,64 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  console.log(req.body);
+  if (id > tours.length) {
+    res.status(404).json({
+      status: "failed",
+      message: "Requested Tour not found",
+    });
+  }
+  res.status(200).json({
+    status: "Success",
+    message: "<Resquested Tour has been upodated>",
+  });
+};
+
+const deleteTour = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  if (id > tours.length) {
+    res.status(404).json({
+      status: "Not Found",
+      message: "Tour not found",
+    });
+  }
+  res.status(204).json({
+    status: "Success",
+    data: null,
+  });
+};
+
+const home = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to Natours",
+  });
+};
+
+// Route Handlers
+
+app.route("/").get(home);
+
+app
+  .route(`/api/v1/tours/:id`)
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+
+// app.patch("/api/v1/tours/:id", );
+
+// app.delete("/api/v1/tours/:id", );
 
 const port = 3000;
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Application runing on port", +port, ".....");
 });
