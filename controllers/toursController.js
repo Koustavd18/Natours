16c76aa6c308080b68a3980169ca14de69b4d155
Koustav Das <kdkdas8@gmail.com>
2024-01-20 13:38:27 +0530

    Natours

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,15 +1,66 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
 const Tour = require("../models/tourModel");
-// const fs = require("fs");
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-// );
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAvarage,price";
+  req.query.fields = "name,price,ratingsAvarage,summary,difficulty";
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   console.log("API requested at:", req.requestTime);
 
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+
+    // 1)Filter
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // 2)Advanced Filter
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = JSON.parse(
+      queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`),
+    );
+
+    let query = Tour.find(queryStr);
+
+    // 3)Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //4)Fields
+    if (req.query.fields) {
+      const field = req.query.fields.split(",").join(" ");
+      query = query.select(field);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //5)Pagination
+
+    const { page } = req.query || 1;
+    const { limit } = req.query || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error("PageNotFound");
+      }
+    }
+
+    const tours = await query;
+
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
@@ -18,11 +69,11 @@ exports.getAllTours = async (req, res) => {
         tours,
       },
     });
-  } catch (err) {
+  } catch (Error) {
     res.status(404).json({
       status: "Failed",
-      message: "inki Pinky Ponky",
-      error: err,
+      message: "Inki Pinky Ponky",
+      error: Error,
     });
   }
 };
@@ -87,7 +138,7 @@ exports.updateTour = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
-      status: "FAiled",
+      status: "Failed",
       message: "Donkey Die ",
       error: err,
     });
