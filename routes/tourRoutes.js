b3d452ba29ed239ea092db60050d1e53133c875c
Koustav Das <kdkdas8@gmail.com>
2024-04-04 12:39:29 +0530

    Updates

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,53 +1,59 @@
-const express = require("express");
-const tourController = require("../controllers/toursController");
-const authController = require("../controllers/authController");
-const reviewRouter = require("./reviewRoutes");
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
-// router.param("id", tourController.checkId);
+// router.param('id', tourController.checkID);
+
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
-  .route("/top-5-cheap")
+  .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route("/tour-stats").get(tourController.getTourStats);
+router.route('/tour-stats').get(tourController.getTourStats);
 router
-  .route("/monthly-plan/:year")
+  .route('/monthly-plan/:year')
   .get(
-    authController.restrictTo("admin", "lead-guide", "guide"),
-    tourController.getMonthlyPlan,
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
   );
-router.route("/tour-name/:id").get(tourController.getTourName);
 
 router
-  .route("/tours-within/:distance/center/:latlng/unit/:unit")
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
+// /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
 
-router.get("/distances/:latlng/unit/:unit", tourController.getDistances);
-
-router.use(authController.protect);
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
 router
-  .route("/")
-  .get(authController.protect, tourController.getAllTours)
+  .route('/')
+  .get(tourController.getAllTours)
   .post(
-    authController.restrictTo("admin", "lead-guide"),
-    tourController.createTour,
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
   );
 
 router
-  .route(`/:id`)
+  .route('/:id')
   .get(tourController.getTour)
   .patch(
-    authController.restrictTo("admin", "lead-guide"),
-    tourController.updateTour,
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
   )
   .delete(
-    authController.restrictTo("admin", "lead-guide"),
-    tourController.deleteTour,
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
   );
-
-router.use("/:tourId/reviews", reviewRouter);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,35 +1,33 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-const authController = require("../controllers/authController");
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
-router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword/:token", authController.resetPassword);
-router.patch(
-  "/updatePassword",
-  authController.protect,
-  authController.updatePassword,
-);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
+// Protect all routes after this middleware
 router.use(authController.protect);
 
-router.route("/me").get(userController.getMe, userController.getUser);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.patch("/updateMe", userController.updateMe);
-router.delete(
-  "/deleteUser",
+router.use(authController.restrictTo('admin'));
 
-  authController.restrictTo("admin"),
-  userController.deleteUser,
-);
-
-router.route("/").get(userController.getAllUsers);
 router
-  .route("/:id")
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
